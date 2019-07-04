9e2a33f58d0f6c0d3674679e0b04154c4a38791f
Natali Toumarkyn <natalit@hackerucrm.co.il>
2019-07-04 10:18:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let addText = function(text) {
+    let heading = document.getElementById('heading');
+    let simpleText = document.createTextNode(text);
+    heading.appendChild(simpleText);
+}
+addText ('Hello word');
