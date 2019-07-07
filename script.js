bf51ccf95b01c179d7732584b12d79361f8965d5
Natali Toumarkyn <natalit@hackerucrm.co.il>
2019-07-07 15:51:23 +0300

    1

--- script.js
+++
@@ -1,6 +0,0 @@
-let addText = function(text) {
-    let heading = document.getElementById('heading');
-    let simpleText = document.createTextNode(text);
-    heading.appendChild(simpleText);
-}
-addText ('Text here');
